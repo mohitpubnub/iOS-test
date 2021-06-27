eb6da63b71e65df71b9696533708e5591689c8a2
Mohit Tejani <mohit.tejani@pubnub.com>
2021-06-27 23:02:24 +0530

    simple app and test command in workflow

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Mohit Tejani on 24/06/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
